65f7a54b12233e9d772e9e4b076dbc72d0af2563
Dongyu WANG <dongyu.wang@monstar-lab.com>
2022-04-25 16:35:40 +0900

    update with LIFF 2.19 feature

---
+++ src/App.svelte
@@ -0,0 +1,212 @@
+<script>
+  import liff from "@line/liff";
+  import Geolocation from "svelte-geolocation";
+  import moment from "moment";
+  import LIFFInspectorPlugin from "@line/liff-inspector";
+  import { LiffMockPlugin } from "@line/liff-mock";
+
+  import { onMount } from "svelte";
+
+  liff.use(new LiffMockPlugin());
+
+  liff.$mock.set((p) => ({
+    ...p,
+    getProfile: { displayName: "Cony *Mocked", userId: "1234" },
+  }));
+
+  let coords = [0, 0];
+  let profile = {};
+  let errorMessage = "";
+
+  async function init() {
+    return await liff.init({
+      liffId: import.meta.env.VITE_LIFF_ID,
+      mock: true,
+    });
+  }
+
+  let promise = init();
+
+  const closeWindow = () => {
+    if (!liff.isInClient()) {
+      window.alert(errorMessage);
+    } else {
+      liff.closeWindow();
+    }
+  };
+  const sendMessage = () => {
+    const today = moment().format("MM/DD");
+    const message = [
+      {
+        type: "flex",
+        altText: "this is a flex message",
+        contents: {
+          type: "bubble",
+          size: "giga",
+          body: {
+            type: "box",
+            layout: "vertical",
+            contents: [
+              {
+                type: "box",
+                layout: "horizontal",
+                contents: [
+                  {
+                    type: "image",
+                    url: "https://upload.wikimedia.org/wikipedia/commons/thumb/1/1b/Svelte_Logo.svg/199px-Svelte_Logo.svg.png",
+                    margin: "none",
+                    offsetTop: "md",
+                    offsetBottom: "md",
+                    offsetStart: "none",
+                  },
+                ],
+                margin: "none",
+              },
+              {
+                type: "text",
+                text: `Today is ${today} `,
+                align: "center",
+                margin: "xl",
+                size: "xl",
+                color: "#eb336d",
+              },
+              {
+                type: "separator",
+                margin: "xxl",
+              },
+              {
+                type: "box",
+                layout: "vertical",
+                contents: [
+                  {
+                    type: "text",
+                    text: "Good luck with Svelte and LIFF",
+                    wrap: true,
+                    align: "center",
+                  },
+                ],
+                borderColor: "#D3D3D3",
+                borderWidth: "normal",
+                cornerRadius: "sm",
+                paddingAll: "lg",
+                margin: "xxl",
+              },
+            ],
+          },
+          styles: {
+            footer: {
+              separator: true,
+            },
+          },
+        },
+      },
+    ];
+    if (!liff.isInClient()) {
+      window.alert(errorMessage);
+    } else {
+      liff
+        .sendMessages(message)
+        .then(() => {
+          liff.closeWindow();
+        })
+        .catch((error) => {
+          window.alert("Error sending message: " + error);
+        });
+    }
+  };
+  onMount(async () => {
+    if (!liff.isInClient()) {
+      errorMessage =
+        "The App is not opened in LINE, LIFF function will not work and will use Mocked User Info \n To open in line, use the QR below";
+    }
+    if (liff.isLoggedIn()) {
+      profile = await liff.getProfile();
+    } else {
+      // Run Login and Retrieve mock profile
+      liff.login();
+      profile = await liff.getProfile();
+    }
+  });
+</script>
+
+<svelte:head>
+  <title>LIFF Demo</title>
+</svelte:head>
+<Geolocation getPosition bind:coords />
+<main>
+  <h1>Liff Demo</h1>
+  {#await promise}
+    <p>LIFF init...</p>
+  {:then}
+    <p>LIFF init succeeded.</p>
+    <div class="button">
+      <button on:click={() => sendMessage()}>Send Message</button>
+      <button on:click={closeWindow}>Close</button>
+    </div>
+
+    <hr />
+    <h3>User Info</h3>
+    <ul>
+      <li>
+        <strong>User Name</strong>
+        :<span>{profile.displayName}</span>
+      </li>
+      <li>
+        <strong>GPS</strong>
+        :<span>{JSON.stringify(coords)}</span>
+      </li>
+    </ul>
+    <h4 class="error">{errorMessage}</h4>
+    <hr />
+    <h3>LIFF Info</h3>
+    <ul>
+      <li>
+        <strong>LIFF Browser</strong>
+        :<span>{liff.isInClient()}</span>
+      </li>
+      <li>
+        <strong>Login Status</strong>
+        :<span>{liff.isLoggedIn()}</span>
+      </li>
+      <li>
+        <strong>Language</strong>
+        :<span>{liff.getLanguage()}</span>
+      </li>
+      <li>
+        <strong>OS</strong>
+        :<span>{liff.getOS()}</span>
+      </li>
+      <li>
+        <strong>LIFF Ver</strong>
+        :<span>{liff.getVersion()}</span>
+      </li>
+      <li>
+        <strong>LINE Ver</strong>
+        :<span>{liff.getLineVersion()}</span>
+      </li>
+    </ul>
+    <hr />
+    <h3>LIFF QR Code</h3>
+    <img src="./liff-qr.png" alt="" />
+  {:catch e}
+    <p>LIFF init failed.</p>
+    <p><code>{`${e}`}</code></p>
+  {/await}
+</main>
+
+<style>
+  main {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+  .error {
+    color: red;
+  }
+  ul {
+    list-style-type: none;
+  }
+</style>
